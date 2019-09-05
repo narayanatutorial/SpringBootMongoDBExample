de9a432ae166078a2a0120b62b080f0f7169cc01
narayanatutorial <narayanaswamy.ragi@gmail.com>
2019-09-05 19:16:47 +0530

    initial commit

---
+++ src/main/java/com/narayanatutorial/SpringBootMongoDBExample/SpringBootMongoDbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.narayanatutorial.SpringBootMongoDBExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbExampleApplication.class, args);
+	}
+
+}
